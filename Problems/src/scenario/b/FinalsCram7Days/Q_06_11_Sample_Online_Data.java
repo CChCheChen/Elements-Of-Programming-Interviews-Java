7fade3da81ba236e4dbed63c3da4d8ab184c193e
CChCheChen <chen.lin.0404@gmail.com>
2019-03-14 21:26:00 -0400

    6.11 & 6.12

--- Problems/src/scenario/b/FinalsCram7Days/Q_06_11_Sample_Online_Data.java
+++ Problems/src/scenario/b/FinalsCram7Days/Q_06_11_Sample_Online_Data.java
@@ -1,5 +1,7 @@
 package scenario.b.FinalsCram7Days;
 
+import java.util.Random;
+
 /*
 6.11 SAMPLE OFFLINE DATA
 This problem is motivated by the need for a company to select a random subset of its customers to roll out a new feature to. 
@@ -9,7 +11,8 @@ without taking the chance of alienating all its users if the roll-out is unsucce
 
 Implement an algorithm that takes as input an array of distinct elements and a size,
 and returns a subset of the given size of the array elements. 
-All subsets should be equally likely. Return the result in input array itself.
+All subsets should be equally likely. 
+Return the result in input array itself.
 
 Hint: How would you construct a random subset of size k + 1 given a random subset of size k?
 
@@ -19,8 +22,40 @@ P78(P91)
 public class Q_06_11_Sample_Online_Data {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] input = {3,7,5,11,9,1};
+		int size = 4;
+		
+		randomSumset(input, size);
+		System.out.println("The randomly select subset is: ");
+		for(int i=0; i<size; i++) {
+			System.out.print(input[i] + " ");
+		}
+	}
 
+	
+	//Time: O(size)
+	//Space:O(1)
+	private static void randomSumset(int[] input, int size) {
+		
+		if(size >= input.length)
+			return;
+		
+		Random random = new Random();
+		
+		for(int i=0; i<size; i++) {
+			
+			//Random.nextInt(RightBoundry) randomly returns a int from 0(included) to RightBoundry(excluded)
+			//"input.length-i" makes sure the randomIndex will be in [0, input.length-i)
+			//so i+randomIndex will be in [i, input.length) --> [i, input.length-1] 
+			//which means the next element will be selected from [i, input.length-1]
+			int randomIndex = random.nextInt(input.length-i);
+			
+			int temp = input[i];
+			input[i] = input[i+randomIndex];
+			input[i+randomIndex] = temp;
+		}
+		
+		return;
 	}
 
 }
--- Problems/src/scenario/b/FinalsCram7Days/Q_06_12_Sample_Online_Data.java
+++ Problems/src/scenario/b/FinalsCram7Days/Q_06_12_Sample_Online_Data.java
@@ -1,8 +1,14 @@
 package scenario.b.FinalsCram7Days;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
 /*
 6.12 SAMPLE ONLINE DATA
 This problem is motivated by the design of a packet sniffer that provides a uniform sample of packets for a network session.
+
 Design a program that takes as input a size k, and reads packets, continuously maintaining a uniform random subset of size k of the read packets.
 
 Hint: Suppose you have a procedure which selects k packets from the first n > k packets as specified. How would you deal with the (n + l)th packet?
@@ -13,8 +19,56 @@ P79(P92)
 public class Q_06_12_Sample_Online_Data {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Character> list = new ArrayList<>();
+		list.add('m');
+		list.add('n');
+		list.add('p');
+		list.add('q');
+		list.add('i');
+		list.add('j');
 
+		int k = 3;//uniform random subset size
+		
+		List<Character> subset = randomUnifromSubset(list, k);
+		System.out.println("After all element in list is read, the uniform random subset is " + subset);
+
+	}
+
+	//Time: O(n), n is list size
+	//Space:O(k), k is subset size
+	private static List<Character> randomUnifromSubset(List<Character> list, int k) {
+		
+		if(k >= list.size())
+			return list;
+		
+		Iterator<Character> itr = list.iterator();
+		List<Character> subset = new ArrayList<>();
+		
+		//read the first k element from list
+		for(int i=0; i<k && itr.hasNext(); i++) {
+			subset.add(itr.next());
+		}
+		
+		//1. start of the random selection from current_index to the list_end
+        //2. swap with one of the element from subset
+		int currentIndex = k;//index 0 to k-1 already read and added in subset
+		Random random = new Random();
+		
+		while(itr.hasNext()) {
+			currentIndex++;
+			int randomIndex = random.nextInt(currentIndex);
+			
+			//System.out.print("Next=" + itr.next() + " ");
+			//System.out.print("currentIndex=" + currentIndex + " ");
+			//System.out.print("randomIndex=" + randomIndex + " ");
+			//System.out.println();
+			
+			if(randomIndex < k) {
+				subset.set(randomIndex, itr.next());//make sure swap only happen when randomIndex is in [0, k-1]
+			}
+		}
+
+		return subset;
 	}
 
 }
