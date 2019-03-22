54dba46105e8ec1e94c77410c2556b8f30e8a348
CChCheChen <chen.lin.0404@gmail.com>
2019-03-21 22:22:57 -0400

    2 more to go

---
+++ Problems/src/scenario/b/FinalsCram7Days/Q_21_16_Recommendation_System.java
@@ -0,0 +1,24 @@
+package scenario.b.FinalsCram7Days;
+
+
+/*
+21.16 DESIGN A RECOMMENDATION SYSTEM
+
+Jingle wants to generate more page views on its news site. 
+A product manager has the idea to add to each article a side-bar of click-able snippet from articles that are likely to be of interest to someone reading the current article.
+
+Design a system that automatically generates a side-bar of related articles.
+
+Hint: This problem can be solved with various degrees of algorithmic sophistication: none at all, simple frequency analysis, or machine learning.
+
+P405(P418)
+
+ */
+public class Q_21_16_Recommendation_System {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
